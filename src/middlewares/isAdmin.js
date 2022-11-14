1918485a46ee4e697db4516ca26d7ce27539a17b
César Pérez <abog.cesarperez@gmail.com>
2022-11-14 15:57:46 -0300

    admin

--- src/middlewares/isAdmin.js
+++ src/middlewares/isAdmin.js
@@ -1,11 +1,11 @@
 //import user from "../daos/usuario/user.Dao.js"
-import user from "../services/user.service.js"
+import user from "../services/user.service.js";
 export default async (req, res, next) => {
- const userNameToFilter=req.user.username
+  const userNameToFilter = req.user.username;
 
-  const userToCompare=await user.getUser({username:userNameToFilter})
- 
-  const isAdmin=userToCompare.admin
+  const userToCompare = await user.getUser({ username: userNameToFilter });
+
+  const isAdmin = userToCompare.admin;
   if (isAdmin) next();
-  else res.json({mensaje:"No tienes credenciales para esta operacion"});
+  else res.json({ mensaje: "No tienes credenciales para esta operacion" });
 };
